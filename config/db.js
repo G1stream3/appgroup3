03e18922ae9f640c57f3046dc85e0620f7e7af88
Nuyoah3k1ko <101079998+Nuyoah3k1ko@users.noreply.github.com>
2022-06-01 02:05:33 +1200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: falue,
+      useUnifiedTopology: true,
     });
 
     console.log("MongoDB is Connected...");
